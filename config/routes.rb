b8caefa80b1c5c8a4827aac6f396b45b246ec59d
Brenna Stuart <brennacodes@gmail.com>
2022-07-05 23:04:29 -0600

    Add cases routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get '/agencies', to: 'agencies#index'
   get '/agencies/:id', to: 'agencies#show'
+  get '/cases', to: 'cases#index'
+  get '/cases/:id', to: 'cases#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
