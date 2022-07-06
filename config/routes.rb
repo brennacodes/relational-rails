ab9be242a147568adfd33503cb9d77a9b062e9ae
Brenna Stuart <brennacodes@gmail.com>
2022-07-05 21:29:53 -0600

    Add agencies index/show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  get '/agency/:id', to: 'agency#index'
+  get '/agencies', to: 'agencies#index'
+  get '/agencies/:id', to: 'agencies#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
