c67477f7aea0796780aa84fe83852482830aa8f2
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-07 21:27:42 -0600

    Update cases routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :departments
 
-  get '/cases', to: 'cases#index', as: 'cases'
-  post '/cases', to: 'cases#create'                   
-  get '/cases/new', to: 'cases#new', as: 'new_case'
-  get '/cases/:id/edit', to: 'cases#edit', as: 'edit_case'
-  get '/cases/:id', to: 'cases#show', as: 'case'
-  patch '/cases/:id', to: 'cases#update'
-  put '/cases/:id', to: 'cases#update'
-  delete '/cases/:id', to: 'cases#destroy'
+  get '/investigations', to: 'investigations#index', as: 'investigations'
+  post '/investigations', to: 'investigations#create'                   
+  get '/investigations/new', to: 'investigations#new', as: 'new_investigation'
+  get '/investigations/:id/edit', to: 'investigations#edit', as: 'edit_investigation'
+  get '/investigations/:id', to: 'investigations#show', as: 'investigation'
+  patch '/investigations/:id', to: 'investigations#update'
+  put '/investigations/:id', to: 'investigations#update'
+  delete '/investigations/:id', to: 'investigations#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
