4a8eda29b8004d1d20a8beb588a68cf171fd189f
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-07 21:56:02 -0600

    Update invest show erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Case.destroy_all
+Investigation.destroy_all
 Department.destroy_all
 
 fbi = Department.create!(name: 'Federal Bureau of Investigations', address: "010 Federal Way, Washington D.C. 80989", jurisdiction: "United States of America", active_cases: 964, is_federal: true)
@@ -15,20 +15,20 @@ atf = Department.create!(name: 'Bureau of Alcohol, Tobacco, Firearms, and Explos
 dea = Department.create!(name: 'Drug Enforcement Agency', address: "666 Dare Drive, Washington D.C. 80989", jurisdiction: "United States of America", active_cases: 964, is_federal: true)
 usm = Department.create!(name: 'U.S. Marshals', address: "123 Howdy Junction, Washington D.C. 80989", jurisdiction: "United States of America", active_cases: 964, is_federal: true)
 
-fbi.cases.create!(subject: 'Missing Person', uid: "abalke83792ks1001", active: true, active_leads: 345)
-fbi.cases.create!(subject: 'Wanted Fugitive', uid: "abalke83792ky78991", active: true, active_leads: 524)
+fbi.investigations.create!(subject: 'Missing Person', uid: "abalke83792ks1001", active: true, active_leads: 345)
+fbi.investigations.create!(subject: 'Wanted Fugitive', uid: "abalke83792ky78991", active: true, active_leads: 524)
 
-upd.cases.create!(subject: 'Robbery', uid: "updlke83792ks1001", active: true, active_leads: 2301)
-upd.cases.create!(subject: 'Assault', uid: "updlke83792ks10181", active: false, active_leads: 2301)
+upd.investigations.create!(subject: 'Robbery', uid: "updlke83792ks1001", active: true, active_leads: 2301)
+upd.investigations.create!(subject: 'Assault', uid: "updlke83792ks10181", active: false, active_leads: 2301)
 
-doj.cases.create!(subject: 'Cyber Crimes', uid: "dojlke83792ks1001", active: true, active_leads: 940)
-doj.cases.create!(subject: 'Domestic Terorism', uid: "dojlke83792ka1501", active: true, active_leads: 240)
+doj.investigations.create!(subject: 'Cyber Crimes', uid: "dojlke83792ks1001", active: true, active_leads: 940)
+doj.investigations.create!(subject: 'Domestic Terorism', uid: "dojlke83792ka1501", active: true, active_leads: 240)
 
-atf.cases.create!(subject: 'Rape, Falure to appear, Firearms violations', uid: "atflke83792ks1001", active: true, active_leads: 540)
-atf.cases.create!(subject: 'Felon in Possession of a Firearm', uid: "atflke83792ks1001", active: true, active_leads: 240)
+atf.investigations.create!(subject: 'Rape, Falure to appear, Firearms violations', uid: "atflke83792ks1001", active: true, active_leads: 540)
+atf.investigations.create!(subject: 'Felon in Possession of a Firearm', uid: "atflke83792ks1001", active: true, active_leads: 240)
 
-dea.cases.create!(subject: 'Money Laundering', uid: "dealke83792ks1001", active: true, active_leads: 340)
-dea.cases.create!(subject: 'Murder: Second Degree ', uid: "dealke83792ks3531", active: true, active_leads: 240)
+dea.investigations.create!(subject: 'Money Laundering', uid: "dealke83792ks1001", active: true, active_leads: 340)
+dea.investigations.create!(subject: 'Murder: Second Degree ', uid: "dealke83792ks3531", active: true, active_leads: 240)
 
-usm.cases.create!(subject: 'Major Fugitive', uid: "usmlke83792ks1001", active: true, active_leads: 500)
-usm.cases.create!(subject: 'Federal Firearms Violations', uid: "usmlke83792ks126a", active: true, active_leads: 640)
+usm.investigations.create!(subject: 'Major Fugitive', uid: "usmlke83792ks1001", active: true, active_leads: 500)
+usm.investigations.create!(subject: 'Federal Firearms Violations', uid: "usmlke83792ks126a", active: true, active_leads: 640)
