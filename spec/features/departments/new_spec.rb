074083f6174c155b46062f8f04c8ab7c6d75d34f
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-10 09:50:51 -0600

    Test: US11 form renders on page; pass

--- spec/features/departments/new_spec.rb
+++ spec/features/departments/new_spec.rb
@@ -6,48 +6,37 @@ RSpec.describe "departments index page" do
     visit "/departments/new"
   end
   
-  it "renders a new department form" do
-    # expect(page).to have_rendered('new')
-    # click_button "Create Department"
-    # expect(page).to have_rendered('new')
-    # expect(page).to have_field(name)
-    # fill_in('Name', with: "CIA")
-    # fill_in 'Address', with: "010 Federal Way, Washington D.C. 80989"
-    # fill_in 'Jurisdiction', with: "United States of America"
-    # fill_in 'Active Cases', with: 964
-    # fill_in 'Is Federal', with: true
-    # expect(page).to have_field(name)
-    # expect(page).to have_field("Address")
-    # expect(page).to have_field("Jurisdiction")
-    # expect(page).to have_field("Active Cases")
-    # expect(page).to have_field("Is Federal")
-    # expect(current_path).to eq("/departments")
-    # expect(page).to have_content("CIA")
-  end
-
-  describe "Navbar Links", type: :feature do
-    describe "Navigation should be available" do
-      it "should include links" do
-        page.should have_link('Departments')
-        page.should have_link('Investigations')
-      end
-
-      it "has links that point to the correct pages" do
-        expect(page).to have_link('Departments', href: '/departments')
-        expect(page).to have_link('Investigations', href: '/investigations')
+    describe "Navbar Links", type: :feature do
+      describe "Navigation should be available" do
+        it "should include links" do
+          page.should have_link('Departments')
+          page.should have_link('Investigations')
+        end
+  
+        it "has links that point to the correct pages" do
+          expect(page).to have_link('Departments', href: '/departments')
+          expect(page).to have_link('Investigations', href: '/investigations')
+        end
       end
     end
+  
+  it "renders a new department form" do
+    fill_in 'department[name]', with: "CIA"
+    fill_in 'department[address]', with: "010 Federal Way, Washington D.C. 80989"
+    fill_in 'department[jurisdiction]', with: "United States of America"
+    fill_in 'department[active_cases]', with: 964
+    select 'True', from: 'department_is_federal'
   end
-
-  xit "successfully creates a new department", type: :feature do
-    # fill_in 'Name', with: "CIA"
-    # fill_in 'Address', with: "010 Federal Way, Washington D.C. 80989"
-    # fill_in 'Jurisdiction', with: "United States of America"
-    # fill_in 'Active Cases', with: 964
-    # fill_in 'Is Federal', with: true
-    # click_button "Create Department"
-    # it { should have_http_status(:ok) }
-    # it { should render_template("departments/show") }
-    # it { should have_content("CIA") }
+  
+  it "successfully creates a new department", type: :feature do
+    fill_in 'department[name]', with: "CIA"
+    fill_in 'department[address]', with: "010 Federal Way, Washington D.C. 80989"
+    fill_in 'department[jurisdiction]', with: "United States of America"
+    fill_in 'department[active_cases]', with: 964
+    select 'True', from: 'department_is_federal'
+    click_button 'save'
+    it { should have_http_status(:ok) }
+    it { should render_template("departments/show") }
+    it { should have_content("CIA") }
   end
 end
