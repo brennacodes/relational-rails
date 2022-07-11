f8e699abef394a752960a62b3aec364eb0250fce
Brenna Stuart <98294995+brennacodes@users.noreply.github.com>
2022-07-10 19:20:04 -0600

    Feat: add working toggle headers for sorting with icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
+
+  def arrow
+    if params[:direction] == 'asc'
+        image_tag("caret-up-fill.svg", alt: "up arrow", size: "15x15")
+    elsif params[:direction] == 'desc'
+        image_tag("caret-down-fill.svg", alt: "down arrow", size: "15x15")
+    else
+        # either a blank image to show or just force no-display of image
+    end
+end
 end
